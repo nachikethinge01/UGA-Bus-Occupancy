8b79dc56e55edf0d08845b373be7c58be0e582cf
nachikethinge01 <nachikethinge1@hotmail.com>
2021-11-14 15:53:05 -0500

    Update Bus-Occupancy.py

--- Bus-Occupancy.py
+++ Bus-Occupancy.py
@@ -7,5 +7,19 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import simpledialog
 
+WEEKENDER = 2620
 
-print("Something!")
+url = "https://routes.uga.edu/Route/" + str(WEEKENDER) + "/Vehicles"
+
+r = requests.get(url)
+data = r.json()
+print(data)
+list_data = list(data)
+
+bus1 = list_data[1]
+
+bus1_list = list(bus1)
+print(bus1_list[2])
+
+
+
